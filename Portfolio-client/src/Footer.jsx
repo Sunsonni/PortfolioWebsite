ad02088d79060151dbb228e5e629103adc143cc4
sunsonni <sonniemnguyen@gmail.com>
2025-02-25 22:31:19 -0600

    haha

--- Portfolio-client/src/Footer.jsx
+++ Portfolio-client/src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <div className='footer-content'>
-                <p>@ 2024 Sonnie M. Nguyen -  St. Louis, MO </p>
+                <p>@ 2025 Sonnie M. Nguyen -  St. Louis, MO </p>
                 <a className='link' id='email-button' href='mailto:sonniemnguyen@gmail.com'>Email Me</a>
                 <a href='https://www.linkedin.com/in/sonnie-nguyen/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
                 <a href='https://github.com/Sunsonni' target='_blank' rel='noopener noreferrer'>Github</a>
