bf382d9f8a3219257331016bc08309a4a12fb68b
sunsonni <sonniemnguyen@gmail.com>
2025-03-13 15:19:04 -0500

    fixed nav bar looking wack

--- Portfolio-client/src/components/About.jsx
+++ Portfolio-client/src/components/About.jsx
@@ -3,27 +3,27 @@ const About = () => {
     
     return (
       <>
+      <h1 className='aboutTitle'>Behind the Keyboard</h1>
       <div className='aboutContent'>
-        <h1>Behind the Keyboard</h1>
-        <p>Hi, I’m Sonnie Nguyen, a Computer Science student at Southern Illinois University Edwardsville.</p> 
-        <h5>Why coding?</h5>
+        <h3>Why coding?</h3>
         <p>
         I’ve been surrounded by technology since I was little and always loved solving problems. Whenever technology came into play, my family would come to me to fix it. I was their personal IT getting viruses off of their computers since middle school. 
 
         Interestingly enough I didn’t automatically go into computer science. I did some small coding projects on my own but I never formally learned programming until I went through a LaunchCode course in 2024. Now I’m back in school getting my degree in Computer Science at SIUE.
         </p>
-        <h5>What are you currently learning and exploring? </h5>
+        <h3>What are you currently learning and exploring? </h3>
         <p>
-        Formally in school, I’m learning about how computers read code and C++ concepts but personally I’ve been playing around with AI and chat bots. I’ve been working on a Gemini based chatbot, toying with the idea of gamifying it and adding a relationship value to the ‘character’ that increases or decreases depending on how the ‘character likes the response. 
+        Formally in school, I’m learning about how computers read code and C++ concepts but personally I’ve been playing around with AI and chat bots. I’ve been working on a Gemini based chatbot, toying with the idea of gamifying it and adding a relationship value to the ‘character’ that increases or decreases depending on how the ‘character’ likes the response. 
         </p>
-        <h5>What’s your process for problem-solving?</h5>
+        <h3>What’s your process for problem-solving?</h3>
         <p>
-        You probably aren’t actually curious but I’ll answer anyway.
-
-        I am a stringent writer. Meaning that I write it down and try to understand what exactly it is I’m trying to do. I find that when I don’t write things down, it’s easy to get bogged down by the details and forget the main focus. 
+        I write things down and try to understand what exactly it is I’m trying to do. Most of the time it's scribbles that only I understand. I find that when I don’t write things down, it’s easy to get bogged down by the details and forget the main focus.
+        <br/>
+        <br/>
         So I write it down. 
         Then I come up with possible solutions and do them.
-        Sometimes they work and sometimes they don’t.</p>
+        Sometimes they work and sometimes they don’t.
+        </p>
       </div>
       </>
     );
--- Portfolio-client/src/components/Card.jsx
+++ Portfolio-client/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
     Card,
     CardBody,
@@ -8,25 +8,33 @@ import {
 } from 'reactstrap'
 
 const Blog = ({project}) => {
-const { title, description, link, id } = project;
-console.log(id);
+    const { title, description, link, id } = project;
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/post/${id}`);
+    };
+
     return (
-        <Card className='card'>
-            <CardTitle>
-                {title}
-            </CardTitle>
-            <CardBody>
-                <CardText>
-                    {description}
-                </CardText>
-            </CardBody>
-            <Link href={link} target="_blank" rel="noopener noreferrer">
-                Github Repo Link
-            </Link>
-            <Link to={`/post/${id}`} className="btn btn-primary">
-            View More
-            </Link>
-        </Card>
+        <div className='project-card' onClick={handleClick}>
+            <Card className='card'>
+                <CardTitle className='nav-link'>
+                    {title}
+                </CardTitle>
+                <CardBody>
+                    <CardText>
+                        {description}
+                    </CardText>
+                </CardBody>
+                <div className='cardButtons'>
+                <a className='projects-link'href={link} target="_blank" onClick={(e) => e.stopPropagation()} >
+                    Github Repo Link
+                </a>
+                <Link to={`/post/${id}`} className="projects-link" onClick={(e) => e.stopPropagation()} >
+                View More
+                </Link>
+                </div>
+            </Card>
+        </div>
     );
 };
 
