8e5885f7cc676e9245de52f9b14e3b64a8d21436
sunsonni <sonniemnguyen@gmail.com>
2025-09-01 23:53:29 -0500

    whoops

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,7 +1,7 @@
-export async function handler(event, context) {
+export async function handler() {
    try {
-      const API_KEY = process.env.STRAPI_API_KEY;// eslint-disable-line
-      const BASE_URL = process.env.STRAPI_AUTH_DOMAIN; // eslint-disable-line
+      const API_KEY = process.env.VITE_STRAPI_API_KEY;// eslint-disable-line
+      const BASE_URL = process.env.VITE_STRAPI_AUTH_DOMAIN; // eslint-disable-line
       const response = await fetch(`${BASE_URL}/api/articles`, {
             headers: {
                 'Authorization': `Bearer ${API_KEY}`,
