431fcc0a1a384d9141e433b83c2215a7227e090c
sunsonni <sonniemnguyen@gmail.com>
2025-09-30 13:27:28 -0500

    changing to curr resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import '../App.css'
 
 const Resume = () => {
-    const pdfUrl = '2025 Resume - Sonnie Nguyen.pdf';
+    const pdfUrl = 'Resume - Sonnie Nguyen.pdf';
     const onButtonClick = () => {
         const link = document.createElement('a');
         link.href = pdfUrl;
-        link.download = "2025 Resume - Sonnie Nguyen.pdf";
+        link.download = 'Resume - Sonnie Nguyen.pdf';
         document.body.appendChild(link);
         link.click();
         document.doctype.removeChild(link);
@@ -15,7 +14,7 @@ const Resume = () => {
     return (
         <>
         <div className='resume-container'>
-            <embed src={pdfUrl} width="80%" align="middle" height="600px" type="application/pdf" />
+            <embed src={pdfUrl} width="80%" height="600px" type="application/pdf" />
         </div>
         <button  className='downloadButton' onClick={onButtonClick}>Download resume here</button>
         </>
